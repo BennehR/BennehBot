84bb1dd04bca94cb96d2cced36749cb541d08c74
BennehR <ben.c.rendall@gmail.com>
2018-01-28 20:32:45 +0000

    Added a .gitignore file - Fleshed out the factorio version check to run on a loop and check against known versions in the database

--- botv2.py
+++ botv2.py
@@ -6,6 +6,7 @@ import sqlite3 as lite
 import sys
 import urllib.request
 import requests
+import os
 
 from bs4 import BeautifulSoup
 from discord.ext import commands
@@ -13,6 +14,7 @@ from discord.ext import commands
 
 con = None
 bot = commands.Bot(command_prefix='!')
+FacVers = []
 
 try:
     print('Opening connection to BennehBotDB database')
@@ -42,21 +44,74 @@ def getToken():
         con.close()
         return(clientToken)
 
+async def FactorioVerListSetup():
+    FacVers.clear()
+    try:
+        con = lite.connect('BennehBotDB.db')
+        cur = con.cursor()
+        cur.execute("SELECT Ver FROM Factorio_Versions")
+        i = 0
+
+        for versions in cur:
+            FacVers.append(versions[0])
+
+    except lite.Error as e:
+        return('Unhandled connection error \n' + e.message)
+    finally:
+        con.close()
+    
+    #print(FacVers)
+    #print('----------')
+
 async def FactorioVersionCheck():
-    page = requests.get('https://forums.factorio.com/viewforum.php?f=3&sid=9e666eb4cc7efaa762351041e014425f')
-    data = page.text
-    soup = BeautifulSoup(data, 'html.parser')
-    links = soup.find_all('a', {"class" : "topictitle"})
-    i = 0
+    while True:
+        await FactorioVerListSetup()
+        Channel = bot.get_channel(396716668967059468)
+        UrlHeader = 'https://forums.factorio.com/'
+        page = requests.get('https://forums.factorio.com/viewforum.php?f=3&sid=9e666eb4cc7efaa762351041e014425f')
+        data = page.text
+        soup = BeautifulSoup(data, 'html.parser')
+        links = soup.find_all('a', {"class" : "topictitle"})
+        i = 0
 
-    for thread in links:
-        charUrl = thread.text
+        #Loop through the links found by BS
+        for thread in links:
+            ThreadName = thread.text
 
-        if charUrl != "":
-            if "Version" in charUrl:
-                ThreadName = charUrl[8:]
-                print(ThreadName)
-                await asyncio.sleep(2)
+            #If the ThreadName is not blank
+            if ThreadName != "":
+
+                #If the ThreadName contains 'Version' trim it out and begin
+                if "Version" in ThreadName:
+                    ThreadName = ThreadName[8:]
+
+                    #If the number does not exist in the array of known versions
+                    #Grab its URL and date submitted then add all this to the DB
+                    if ThreadName not in FacVers:
+                        print(ThreadName + " appears to be a new release")
+                        ThreadURL = thread.attrs['href']
+                        ThreadURL = UrlHeader + ThreadURL[2:]
+                        ThreadSubmitted = thread.parent.text
+                        SplitLoc = ThreadSubmitted.find('»')
+                        ThreadSubmitted = ThreadSubmitted[SplitLoc + 2:]
+                        ThreadSubmitted = ThreadSubmitted.strip()
+
+                        try:
+                            con = lite.connect('BennehBotDB.db')
+                            cur = con.cursor()
+                            cur.execute("INSERT INTO Factorio_Versions VALUES(?,?,?,?)", (ThreadName, ThreadSubmitted, ThreadURL, "N/A"))
+                            con.commit()
+                        except lite.Error as e:
+                            return('Unhandled connection error \n' + e.message)
+                        finally:
+                            con.close()
+                        
+                        await Channel.send('A new version has been posted to the forum.')
+                        await Channel.send(ThreadName + " - " + ThreadSubmitted + " - " + ThreadURL)
+                        
+        print("Done, waiting")
+        await asyncio.sleep(900)
+        print("Finished waiting")
 
 @bot.event
 async def on_ready():
@@ -64,6 +119,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------') 
+    await FactorioVerListSetup()
     bot.loop.create_task(FactorioVersionCheck())
 
 @bot.command(pass_context=True)
